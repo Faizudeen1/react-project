262e638b43ff38329b336b7f9515b110473c6083
Faizudeen1 <114797538+Faizudeen1@users.noreply.github.com>
2024-10-06 11:42:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
   }
 
   return (
-    <Router>
+    <Router basename="/React-Project"> {/* Add basename here */}
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
